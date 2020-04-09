85ffdf04a55deed8228c3e8b7db51d78cbc24c01
Aubrey Sambor <aubrey@star-shaped.org>
2020-04-09 18:41:40 -0400

    Updated date filter stuff

--- src/filters/formatted-date-filter.js
+++ src/filters/formatted-date-filter.js
@@ -1,6 +1,6 @@
 const moment = require('moment');
 
 module.exports = function dateFilter(value) {
-  const dateObject = moment(value).add(6, 'hour').format('MMM Do, YYYY');
+  const dateObject = moment(value).add(6, 'hour').format('MMMM Do, YYYY');
   return dateObject;
 };
