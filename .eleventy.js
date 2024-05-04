9997f4c3e720536f8dbf880a29f4323996b32bdb
Aubrey Sambor <aubrey@star-shaped.org>
2024-05-04 09:37:17 -0400

    Update everything because why not

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 // Import @11ty plugins
 const rssPlugin = require('@11ty/eleventy-plugin-rss');
 const markdownIt = require('markdown-it');
-const markdownItEleventyImg = require("markdown-it-eleventy-img");
+// const markdownItEleventyImg = require("markdown-it-eleventy-img");
 
 const { DateTime } = require("luxon");
 
@@ -20,34 +20,34 @@ const parseTransform = require('./src/transforms/parse-transform.js');
 
 module.exports = function (config) {
 
-  config.setLibrary('md', markdownIt ({
-    html: true,
-    breaks: true,
-    linkify: true
-  })
-  .use(markdownItEleventyImg, {
-    imgOptions: {
-      formats: ["webp", "jpeg"],
-      widths: [1024, 500, 300],
-      urlPath: "/images/",
-      outputDir: "./src/images/",
-    },
-    globalAttributes: {
-      sizes: "100vw"
-    },
+  // config.setLibrary('md', markdownIt ({
+  //   html: true,
+  //   breaks: true,
+  //   linkify: true
+  // })
+  // .use(markdownItEleventyImg, {
+  //   imgOptions: {
+  //     formats: ["webp", "jpeg"],
+  //     widths: [1024, 500, 300],
+  //     urlPath: "/images/",
+  //     outputDir: "./src/images/",
+  //   },
+  //   globalAttributes: {
+  //     sizes: "100vw"
+  //   },
 
-    renderImage(image, attributes) {
-      const [ Image, options ] = image;
-      const [ src, attrs ] = attributes;
+  //   renderImage(image, attributes) {
+  //     const [ Image, options ] = image;
+  //     const [ src, attrs ] = attributes;
 
-      Image(src, options);
+  //     Image(src, options);
 
-      const metadata = Image.statsSync(src, options);
-      const imageMarkup = Image.generateHTML(metadata, attrs);
+  //     const metadata = Image.statsSync(src, options);
+  //     const imageMarkup = Image.generateHTML(metadata, attrs);
 
-      return `<figure>${imageMarkup}${attrs.title ? `<figcaption>${attrs.title}</figcaption>` : ""}</figure>`;
-    }
-  }));
+  //     return `<figure>${imageMarkup}${attrs.title ? `<figcaption>${attrs.title}</figcaption>` : ""}</figure>`;
+  //   }
+  // }));
 
   config.addTemplateFormats('css');
 
