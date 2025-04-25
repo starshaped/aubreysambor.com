ed08c1e78de97cbd052c53b933dcd6da32b3dc2f
Aubrey Sambor <aubrey@star-shaped.org>
2025-04-25 14:33:42 -0400

    Add hover and js to speaking cards

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,15 @@
+const cards = document.querySelectorAll('.content__text--speaking li');
 
+cards.forEach((card) => {
+  let down, up, link = card.querySelector('a');
+  if (link) {
+    card.style.cursor = 'pointer';
+    card.onmousedown = () => down = +new Date();
+    card.onmouseup = () => {
+      up = +new Date();
+      if ((up - down) < 200) {
+        link.click();
+      }
+    }
+  }
+});
